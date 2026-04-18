4a3ad8ca1f27284787b09fff277dac35f8d0bcd7
Keshav Patidar <keshavpatidar9876@gmail.com>
2026-04-18 14:03:47 +0000

    fix production cable config

--- config/puma.rb
+++ config/puma.rb
@@ -20,15 +20,15 @@
 #
 # The default is set to 3 threads as it's deemed a decent compromise between
 # throughput and latency for the average Rails application.
-#
 # Any libraries that use a connection pool or another resource pool should
 # be configured to provide at least as many connections as the number of
 # threads. This includes Active Record's `pool` parameter in `database.yml`.
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 3)
 threads threads_count, threads_count
-
-# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT", 3000)
+# Specifies the `port` that Puma will listen on to receive requests; default is 3000.
+
+
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
